1782b7f4a9137b06f90b167f4cd99d4908768538
AndersonJJR <adasilvachavesjunior@gmail.com>
2025-12-16 16:55:53 -0300

    Projeto conversor de moedas

--- src/CoinSearch.java
+++ src/CoinSearch.java
@@ -9,44 +9,67 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CoinSearch {
 
     Gson gson = new Gson();
     ExchangeResponse exchangeResponse = new ExchangeResponse();
     DecimalFormat df = new DecimalFormat("0.00");
+    List<String> moedaFornecida = new ArrayList<>();
+    List<String> moedaConvertida = new ArrayList<>();
+    List<Double> moedaQuantidade = new ArrayList<>();
 
     public void convertCoin(String moedaUser, double quantidade, String moedaConvert){
+
+        String apiKey = "543ac8c9fbcb888a707419cc";
+        String url = "https://v6.exchangerate-api.com/v6/" + apiKey + "/latest/" + moedaUser.toUpperCase();
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+
         try {
-            String apiKey = "543ac8c9fbcb888a707419cc";
-            String url = "https://v6.exchangerate-api.com/v6/" + apiKey + "/latest/" + moedaUser.toUpperCase();
+            HttpResponse<String> response = client.send(request,
+                    HttpResponse.BodyHandlers.ofString());
+            JsonElement element = JsonParser.parseString(response.body());
+            JsonObject objectRoot = element.getAsJsonObject();
 
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(url))
-                    .GET()
-                    .build();
+            exchangeResponse = gson.fromJson(objectRoot, ExchangeResponse.class);
+            double valor = quantidade * exchangeResponse.conversion_rates.getOrDefault(moedaConvert , 0.0);
 
-            try {
-                HttpResponse<String> response = client.send(request,
-                        HttpResponse.BodyHandlers.ofString());
-                JsonElement element = JsonParser.parseString(response.body());
-                JsonObject objectRoot = element.getAsJsonObject();
-                exchangeResponse = gson.fromJson(objectRoot, ExchangeResponse.class);
-                double valor = quantidade * exchangeResponse.conversion_rates.getOrDefault(moedaConvert , 0.0);
-                gson.newBuilder().setPrettyPrinting().create();
+            gson.newBuilder().setPrettyPrinting().create();
 
-                System.out.println("-------------  -------------");
-                System.out.println("Moeda fornecida: " + moedaUser + "\nQuantidade: " + quantidade);
-                System.out.println("-------------  -------------");
-                System.out.println("Moeda convertida: "+ moedaConvert + "\nQuantidade: " + df.format(valor));
+            System.out.println("\n------------- CONVERSÃO -------------");
+            System.out.println("Moeda fornecida: " + moedaUser + "\nQuantidade: " + quantidade);
 
-            } catch (IOException | InterruptedException e) {
-                System.out.println("Não foi possivel verificar a cotação de preços");
-                e.getMessage();
+            System.out.println("\n------------- CONVERSÃO -------------");
+            System.out.println("Moeda convertida: "+ moedaConvert + "\nQuantidade: " + df.format(valor));
+
+            moedaFornecida.add(moedaUser);
+            moedaConvertida.add(moedaConvert);
+            moedaQuantidade.add(quantidade);
+
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Não foi possivel verificar a cotação de preços");
+        }
+    }
+
+    public void mostrarLista() {
+
+        int indice = 1;
+        if (!moedaFornecida.isEmpty()) {
+            for (int i = 0; i < moedaFornecida.size(); i++) {
+                System.out.println( (indice + i) + " Foi-se convertido a moeda " + moedaFornecida.get(i) +
+                        " com a quantidade " + moedaQuantidade.get(i) +
+                        " para a moeda " + moedaConvertida.get(i));
+
             }
-        } catch (Exception e) {
-            e.getMessage();
+        } else {
+            System.out.println("Não existem converções feitas.");
         }
     }
 }
--- src/TextField.java
+++ src/TextField.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TextField {
@@ -7,19 +8,30 @@ public class TextField {
     String moedaConvert;
     String moedaUser;
     double quantidade;
+    int awnser;
 
     public void iniciarDialogo(){
 
         do {
-            System.out.println("-------------  -------------");
+            System.out.println("\n------------- MENU -------------");
             System.out.println("Bem vindo ao conversor de moedas!");
-            System.out.println("Opções disponiveis:\n");
+            System.out.println("Opções disponiveis:");
+            System.out.println("---------------  ---------------\n");
+            System.out.println("---------------  ---------------");
             System.out.println("0. Sair");
             System.out.println("1. Consultar moedas");
             System.out.println("2. Realizar conversão");
+            System.out.println("3. Consultar histórico");
+            System.out.println("---------------  ---------------");
 
-            int awnser = scanner.nextInt();
-            scanner.nextLine();
+            try {
+                awnser = scanner.nextInt();
+                scanner.nextLine();
+            }
+            catch (InputMismatchException e ){
+                System.out.println("Entrada invalida!");
+                scanner.nextLine();
+            }
 
             if (awnser == 0) break;
 
@@ -30,35 +42,59 @@ public class TextField {
                 case 2:
                     opcaoDois();
                     break;
+                case 3:
+                    opcaoTres();
+                    break;
             }
         }
-
         while (true);
     }
 
     public void opcaoUm(){
         System.out.println("------------- MOEDAS -------------");
-        System.out.println("USD | BRL | JPY | GBP | AUD | CAD");
-        System.out.println("CHF | HRK | MXN | NZD | CNH | CZK");
+        System.out.println("USD | BOB | JPY | GBP | AUD | CAD");
+        System.out.println("BRL | ARS | CLP | COP | CNH | CZK");
+        System.out.println("----------------  ----------------");
     }
 
     public void opcaoDois(){
         try {
-
-            System.out.println("Digite a moeda possuida:");
+            System.out.println("Digite a moeda POSSUIDA:");
             moedaUser = scanner.nextLine().toUpperCase();
 
-            System.out.println("Digite a quantidade dessa moeda:");
+            if (moedaUser.length() < 3){
+                System.out.println("Moeda invalida!");
+                opcaoDois();
+            }
+
+            System.out.println("Digite a QUANTIDADE dessa moeda:");
             quantidade = scanner.nextDouble();
             scanner.nextLine();
 
-            System.out.println("Digite a moeda a ser convertida:");
+            if (quantidade < 0){
+                System.out.println("Não há quantia necessária para conversão.");
+                opcaoDois();
+            }
+
+            System.out.println("Digite a moeda a ser CONVERTIDA:");
             moedaConvert = scanner.nextLine().toUpperCase();
 
-            coinSearch.convertCoin(moedaUser, quantidade, moedaConvert);
+            if (moedaConvert.length() < 3){
+                System.out.println("Moeda invalida!");
+                opcaoDois();
+            } else {
+                coinSearch.convertCoin(moedaUser, quantidade, moedaConvert);
+            }
 
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        } catch (InputMismatchException e) {
+            System.out.println("Digite apenas números!");
+            scanner.nextLine();
         }
+    }
+
+    public void opcaoTres(){
+        System.out.println("----------------  ----------------");
+        coinSearch.mostrarLista();
+        System.out.println("----------------  ----------------");
     }
 }
